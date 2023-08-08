9be69624b9105d662b0c59bcfe79d5248ddfad1f
Arch-Noize <santiagonoize@gmail.com>
2023-08-08 07:03:50 -0400

    Add API

---
+++ src/modules/api.js
@@ -0,0 +1,4 @@
+const invAPI = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/dSv5DdTGl6SZHdXDAlEr/';
+const pokeAPI = 'https://pokeapi.co/api/v2/pokemon/';
+
+export { invAPI , pokeAPI };
