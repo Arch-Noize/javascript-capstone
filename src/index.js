eee7a4e47aebdf841af5f5ae438415006b377ec0
Arch-Noize <santiagonoize@gmail.com>
2023-08-11 08:18:14 -0400

    Add display changes

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ const commentSection = document.querySelector("#comment-section");
 const commentBtn = document.querySelector('#comment-btn');
 const newComment = document.querySelector('#add-comment');
 const commentForm = document.querySelector('#comment-form');
-const comments = document.querySelector("#comment");
 
 const reservationSection = document.querySelector("#reservation-section");
 const reservationBtn = document.querySelector("#res-btn");
@@ -44,38 +43,54 @@ commentSection.innerHTML = `
   const ul = document.createElement("ul")
   const form = document.createElement("form")
   
+  const handlePopup = (e) => {
+    if (e.target.id.contains("comment-btn")){
+        console.log("hi, comments!")
+    } else if (e.target.id.contains("res-btn")){
+        console.log("hi, reservation!")
+    }
+}
 
 */
 
 const displayComment = async () => {
-  console.log("hi, this is a comment!")
-  const commentList = await getComment("item5");
-  if (Object.keys(commentList).length = 0){
-    comments.innerHTML = '';
-  } else {
-
-    commentList.forEach((item) => {
-        comments.innerHTML+= `<li class="comment"> ${item.username}: ${item.comment} (${item.creation_date})`
-    })
-  };
+  console.log("hi, this is a comment!");
+  const comments = document.querySelector("#comment");
+  const commentList = await getComment("item3");
+  commentList.forEach((item) => {
+    comments.innerHTML+= `<li class="comment"> ${item.username}: ${item.comment} (${item.creation_date})`
+  });
 }
 
 const displayReservations = async () => {
   console.log("hi, this is a reservation!")
 }
 
-commentBtn.addEventListener('click', () => {
-  displayComment();
+commentBtn.addEventListener('click', async () => {
   popup.classList.remove("overlay");
+  commentSection.innerHTML = `
+    <h3>
+    Comments!
+    </h3>
+    <ul id="comment" class="comment-list">
+
+    </ul>
+    <form id="comment-form">
+        <input type="text" id="username" placeholder="Please leave a name" autocomplete="off">
+        <input type="text" id="comment" placeholder="Please add a comment" autocomplete="off">
+    </form>
+    <button id="add-comment">Comment</button>`
+  displayComment();
 });
+
+reservationBtn.addEventListener('click', () => {
+  popup.classList.remove("overlay");
+  commentSection.innerHTML = "Hello, reservations!"
+})
 
 closeBtn.addEventListener('click', () => {
   popup.classList.add("overlay");
 });
-
-// const commentModal = () => {
-
-// }
 
 newComment.addEventListener('click', (e) => {
     e.preventDefault;
@@ -93,4 +108,4 @@ newComment.addEventListener('click', (e) => {
     }
 });
 
-document.addEventListener("DOMContentLoaded", displayComment());
+// document.addEventListener("DOMContentLoaded", displayComment());
